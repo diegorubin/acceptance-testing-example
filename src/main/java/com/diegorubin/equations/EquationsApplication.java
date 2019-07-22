582d9806d4ecaf3fc7075c2b237f785d71f6c1c7
Diego Rubin <rubin.diego@gmail.com>
2019-07-22 05:46:28 -0300

    creating repo

---
+++ src/main/java/com/diegorubin/equations/EquationsApplication.java
@@ -0,0 +1,13 @@
+package com.diegorubin.equations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquationsApplication.class, args);
+	}
+
+}
